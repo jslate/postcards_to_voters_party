7aad49d29a9b547135a96a30cf087b4fcc1afe73
Jonathan Slate <jslate@epionhealth.com>
2022-01-30 10:30:02 -0500

    upgrade to rails 6

--- config/application.rb
+++ config/application.rb
@@ -11,8 +11,6 @@ module PostcardsToVotersPary
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
 
-    config.hosts << "mysterious-peak-83366.herokuapp.com"
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
