72dcbab53e39f8f20872987b444b09c7f8903cad
Jonathan Slate <jslate@epionhealth.com>
2022-01-30 15:28:41 -0500

    get rid of webpacker

---
+++ app/assets/javascripts/application.js
@@ -0,0 +1,12 @@
+document.getElementById("show_hide")?.addEventListener("click", (e) => {
+  e.preventDefault();
+  const linkElement = e.target;
+  const fullTextElement = document.getElementById("full_text");
+  if (fullTextElement.style.display === "none") {
+    fullTextElement.style.display = "inline-block";
+    linkElement.innerHTML = "▼ Hide full text"
+  } else {
+    fullTextElement.style.display = "none";
+    linkElement.innerHTML = "▶ Show full text"
+  }
+});
